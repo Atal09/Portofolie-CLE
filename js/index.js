3d853092327b8d82679f64e6b032ecb07181f40b
Atal09 <113996359+Atal09@users.noreply.github.com>
2023-04-07 16:00:13 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {
+    var header = document.querySelector("header");
+    if (window.pageYOffset > 0) {
+      header.style.backgroundColor = "#fff";
+      header.style.color = "#102a43";
+    } else {
+      header.style.backgroundColor = "#102a43";
+      header.style.color = "#fff";
+    }
+  };
+
+
+function validateForm() {
+  let firstName = document.getElementById("firstname").value;
+  let lastName = document.getElementById("lastname").value;
+  let email = document.getElementById("email").value;
+  let date = document.getElementById("date").value;
+
+  if (firstName === "" || lastName === "" || email === "" || date === "") {
+            alert("Vul alle verplichte velden in.");
+            return false;
+  }
+        
+  alert("Bedankt voor je bericht!");
+        return true;
+}
