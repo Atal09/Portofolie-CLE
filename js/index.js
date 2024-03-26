ad59f47930d2d240742322f74e4a5bf8927c5f62
Atal09 <113996359+Atal09@users.noreply.github.com>
2024-03-26 10:16:58 +0100

    changes css js dropdown changes, html changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-window.onscroll = function() {
-    let header = document.querySelector("header");
-    if (window.pageYOffset > 0) {
-      header.style.backgroundColor = "#fff";
-      header.style.color = "#102a43";
-    } else {
-      header.style.backgroundColor = "#102a43";
-      header.style.color = "#fff";
-    }
-  };
+// window.onscroll = function() {
+//     let header = document.querySelector("header");
+//     if (window.pageYOffset > 0) {
+//       header.style.backgroundColor = "#fff";
+//       header.style.color = "#102a43";
+//     } else {
+//       header.style.backgroundColor = "#102a43";
+//       header.style.color = "#fff";
+//     }
+//   };
 
 
 function validateForm() {
@@ -23,4 +23,32 @@ function validateForm() {
         
   alert("Bedankt voor je bericht!");
         return true;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let dropdownIcon = document.querySelector('.mobile-dropdown');
+  let nav = document.querySelector('nav');
+
+  dropdownIcon.addEventListener('click', function() {
+      nav.classList.toggle('show-nav'); // Toggle een klasse om de navigatiebalk te tonen/verbergen
+  });
+});
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
